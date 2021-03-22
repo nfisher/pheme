9e46645a45746d5543c6a28f6974ecde2f060d30
Nathan Fisher <nfisher@junctionbox.ca>
2021-03-22 13:44:21 -0400

    Add listener address override

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
-	log.Println("Binding to :8008")
+	addr := flag.String("listen", ":8008", "Override the default listening address")
+	flag.Parse()
+
+	log.Println("Binding to " + *addr)
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
-	log.Fatal(http.ListenAndServe(":8008", nil))
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
